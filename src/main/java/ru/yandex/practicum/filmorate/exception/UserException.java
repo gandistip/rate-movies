0b8b6f703c9218432a3edcfcc337c6cd97106e96
leonid <kuprikovleonid@yandex.ru>
2023-04-28 14:34:02 +0200

    style: добавление пробелов

--- src/main/java/ru/yandex/practicum/filmorate/exception/UserException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/UserException.java
@@ -5,41 +5,35 @@ import org.springframework.web.bind.annotation.RequestBody;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.time.LocalDate;
+
 @Slf4j
 public class UserException {
     public static void validationException(@RequestBody User user) throws ValidationException {
-
         if (user == null) {
             log.debug("ValidationException");
             throw new ValidationException("Запрос не может быть пустым.");
         }
-
-        if(user.getEmail() == null || user.getEmail().isBlank()) {
+        if (user.getEmail() == null || user.getEmail().isBlank()) {
             log.debug("ValidationException");
             throw new ValidationException("Email не может быть пустым.");
         }
-
-        if(!user.getEmail().contains("@")) {
+        if (!user.getEmail().contains("@")) {
             log.debug("ValidationException");
             throw new ValidationException("Email не может быть без @.");
         }
-
-        if(user.getLogin() == null || user.getLogin().isBlank()) {
+        if (user.getLogin() == null || user.getLogin().isBlank()) {
             log.debug("ValidationException");
             throw new ValidationException("Логин не может быть пустым.");
         }
-
-        if(user.getLogin().contains(" ")) {
+        if (user.getLogin().contains(" ")) {
             log.debug("ValidationException");
             throw new ValidationException("Логин не может содержать пробел.");
         }
-
         try {
             if(user.getBirthday().isAfter(LocalDate.now())) {
                 log.debug("ValidationException");
                 throw new ValidationException("Дата рождения не может быть в будущем.");
             }
-        } catch (NullPointerException e) {}
-
+        } catch (NullPointerException e) { System.out.println("NPE"); }
     }
 }
